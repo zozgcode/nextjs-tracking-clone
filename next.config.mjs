859cad435278f887471b6646dfcaf51f754d5817
sunshine-web-coder <webdev4gaming@gmail.com>
2025-04-27 18:32:21 +0100

    gdfd

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "i.imgur.com",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "basho.fueko.net",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export', // Enable static export
+  images: {
+    unoptimized: true, // Required for static HTML export
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "basho.fueko.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
